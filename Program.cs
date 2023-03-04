ca8569635b56d6bb6f28bb6c1027afdba152f19a
Kirill <shadow986@mail.ru>
2023-03-04 22:54:08 +0300

    Задание 41

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// подать на вход числа А и Б и выдать результат возведения А в степень Б
+int ReadNumber (string message)
+{
+    Console.WriteLine (message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int m = ReadNumber ("Введите количество вводимых чисел:");
+int numberB = 0;
+for (int count = 0; count < m; count++)
+{
+    int numberA = ReadNumber("Введите число:");
+    if (numberA > 0)
+    {
+        numberB++;
+    }
+}
+Console.WriteLine ("Количество чисел больше нуля = " + numberB);
+
